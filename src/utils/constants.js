01447d17fb3ac9943adc9e1c9298381fe06ee7ee
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2023-09-27 10:40:14 +0530

    updated validation for dob

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@ export const VALIDATE_EMAIL_REGEX =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 export const VALIDATE_DATE_REGEX =
-  /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+  /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/;
 
 export const REGISTRATION_FORM_FIELDS = {
   name: "",
